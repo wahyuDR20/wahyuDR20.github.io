be162ff087e339950dd26e441e90d88215103c92
wahyudr20 <waysdenim20@gmail.com>
2020-07-14 00:29:02 +0700

    menambah fungsi orig and dest

--- tampil.php
+++ tampil.php
@@ -2,19 +2,19 @@
 require 'curl_lib.php';
 
 /*run curl runction*/
-$str_data = http_request('https://icms.garuda-indonesia.com/Services/Shipment/AWBTrackingService.svc/GetAWBTrackingRecord?BasedOn=0&AWBNo='.$_GET['awbno'].'&CarrierCode='.$_GET['crcode'].'');
+$str_data = http_request('https://icms.garuda-indonesia.com/Services/Shipment/AWBTrackingService.svc/GetAWBTrackingRecord?BasedOn=0&AWBNo=' . $_GET['awbno'] . '&CarrierCode=' . $_GET['crcode'] . '');
 // decoding the data
-$datako = json_decode($str_data, TRUE);
+$datako = json_decode($str_data, true);
 //print_r($datako);
 /*fopen and fwrite to data.json*/
 $fp = fopen('data.json', 'w');
-fwrite($fp,$datako);
+fwrite($fp, $datako);
 /*then close*/
 fclose($fp);
 /*then get curl*/
-$str_dt =  file_get_contents('data.json');
+$str_dt = file_get_contents('data.json');
 /*decode the data*/
-$data= json_decode($str_dt,true);
+$data = json_decode($str_dt, true);
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,7 +26,9 @@ $data= json_decode($str_dt,true);
     <div class="wrapper">
       <div id="formContent">
         <!-- Tabs Titles -->
-        <h2 class="active">AWB NO:<?php echo $_GET['crcode'];?>-<?=$_GET['awbno'];?>
+        <h2 class="active">AWB NO:<?php echo $_GET['crcode']; ?>-<?=$_GET['awbno'];?>
+        <br>
+        <?=$data["Table0"][0]["Origin"];?>-<?=$data["Table0"][0]["Destination"];?>
           <!-- <br>(<?=$data["Table0"][0]["FlightNo"];?>) -->
         </h2>
         <!-- Icon -->
@@ -34,23 +36,22 @@ $data= json_decode($str_dt,true);
           <table>
             <tbody>
               <tr>
-                <?php	
-              
-$a=count($data["Table0"]);
-if($a=='0'){
-echo "data tidak ditemukan, coba cek no awb kembali";
-}else{
-$no=0;
-for($i = 0; $i < sizeof($data["Table0"]); $i++)
-{
-$n2=$no++;
-if($n2!=0){
-echo '<td style="text-align:center;padding:1px;"><img src="images/arrowtrack.png" style="width:50px";" alt="arrow"></td>';
-}
-echo '<td style="text-align:center;padding:1px;"><p>'.$data["Table2"][$i]["FlightNo"].'<br>'.$data["Table2"][$i]["Station"].'</p><img src="images/'.$data["Table2"][$i]["Action"].'.png" style="width:50px";" alt="confirm order">
-<p>'.$data["Table2"][$i]["Action"].'</p><p>'.$data["Table2"][$i]["createdon"].'</p><p>'.$data["Table2"][$i]["Pieces"].' pcs, '.$data["Table2"][$i]["Weight"].'kg</p></td>';
+                <?php
 
-}
+$a = count($data["Table0"]);
+if ($a == '0') {
+    echo "data tidak ditemukan, coba cek no awb kembali";
+} else {
+    $no = 0;
+    for ($i = 0; $i < sizeof($data["Table0"]); $i++) {
+        $n2 = $no++;
+        if ($n2 != 0) {
+            echo '<td style="text-align:center;padding:1px;"><img src="images/arrowtrack.png" style="width:50px";" alt="arrow"></td>';
+        }
+        echo '<td style="text-align:center;padding:1px;"><p>' . $data["Table2"][$i]["FlightNo"] . '<br>' . $data["Table2"][$i]["Station"] . '</p><img src="images/' . $data["Table2"][$i]["Action"] . '.png" style="width:50px";" alt="confirm order">
+<p>' . $data["Table2"][$i]["Action"] . '</p><p>' . $data["Table2"][$i]["createdon"] . '</p><p>' . $data["Table2"][$i]["Pieces"] . ' pcs, ' . $data["Table2"][$i]["Weight"] . 'kg</p></td>';
+
+    }
 }
 ?>
               </tr>
