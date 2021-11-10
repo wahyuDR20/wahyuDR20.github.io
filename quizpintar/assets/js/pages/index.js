451b1ee1e0f3a2afbcbc54d39b65c7adda88b6d5
wahyu <39843380+wahyuDR20@users.noreply.github.com>
2021-11-10 16:13:58 +0700

    Create index.js

---
+++ quizpintar/assets/js/pages/index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+$(document).ready(function () {
+    $('#slider-img').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1170: {
+                items: 4
+            }
+        }
+    });
+});
+        
